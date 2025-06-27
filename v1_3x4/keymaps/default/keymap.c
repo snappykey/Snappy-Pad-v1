8823f96c2a3d8ad7fda97e3097bba2e64dd1e0ef
snappykey <snappykey@gmail.com>
2025-06-27 17:14:44 +0300

    Update keymap.c

--- v1_3x4/keymaps/default/keymap.c
+++ v1_3x4/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-
+// Programmed by Snappy Key CEO
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_F13,   KC_F14, KC_F15, KC_F16,
@@ -73,14 +73,9 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
         layer = 0;
     }
  }
- //if (!gpio_read_pin(B6) && !gpio_read_pin(F6)) {
- //    layer_move(4);
- //    gpio_write_pin_high(F7);
- //    gpio_write_pin_low(B2);
- //    gpio_write_pin_high(B5);
- //    layer--;
- // }
+
  if(!gpio_read_pin(F6)) {
+// You can change "KC_MPLY" to any qmk supported shortcut
      tap_code(KC_MPLY);
  }
     return true;
@@ -94,6 +89,5 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_VOLD);
         }
     }
-
     return false;
 }
